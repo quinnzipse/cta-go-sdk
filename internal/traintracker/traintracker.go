059bddbe78d70cfc9506b7277461b456d84a1680
Quinn Zipse <quinn@quinnzipse.dev>
2026-04-21 20:13:30 -0500

    feat: add support for new api types
    Signed-off-by: Quinn Zipse <quinn@quinnzipse.dev>

--- internal/traintracker/traintracker.go
+++ internal/traintracker/traintracker.go
@@ -1,13 +1,37 @@
 package traintracker
 
+import "net/http"
+
+const DefaultBaseUrl = "https://lapi.transitchicago.com"
+
 type TrainTrackerProps struct {
-	Key string
+	Key           string
+	HttpClient   *http.Client
+	BaseUrl     string
 }
 
 type TrainTracker struct {
-	key string
+	key        string
+	httpClient *http.Client
+	baseUrl   string
 }
 
 func NewTrainTracker(props TrainTrackerProps) TrainTracker {
-	return TrainTracker{key: props.Key}
+	client := props.HttpClient
+	if client == nil {
+		client = http.DefaultClient
+	}
+	baseUrl := props.BaseUrl
+	if baseUrl == "" {
+		baseUrl = DefaultBaseUrl
+	}
+	return TrainTracker{
+		key:        props.Key,
+		httpClient: client,
+		baseUrl:   baseUrl,
+	}
+}
+
+func (tt TrainTracker) Do(req *http.Request) (*http.Response, error) {
+	return tt.httpClient.Do(req)
 }
--- internal/traintracker/types.go
+++ internal/traintracker/types.go
@@ -95,3 +95,137 @@ type Eta struct {
 	Lon     *float64
 	Heading *int16
 }
+
+type PositionsApiResponse struct {
+	Ctatt struct {
+		Tmst  string          `json:"tmst"`
+		ErrCd string          `json:"errCd"`
+		ErrNm string          `json:"errNm"`
+		Route []ApiRoutePos  `json:"route"`
+	} `json:"ctatt"`
+}
+
+type ApiRoutePos struct {
+	Rt       string         `json:"rt"`
+	Position []ApiTrainPos  `json:"position"`
+}
+
+type ApiTrainPos struct {
+	TrainId   string   `json:"trainId"`
+	RunNumber string   `json:"rn"`
+	Rt        string   `json:"rt"`
+	TrDr      string   `json:"trDr"`
+	Lat       string   `json:"lat"`
+	Lon       string   `json:"lon"`
+	Heading   string   `json:"heading"`
+	NextStpId string   `json:"nextStpId"`
+	NextStpNm string   `json:"nextStpNm"`
+	NextStpTm string   `json:"nextStpTm"`
+	PrevStpId string   `json:"prevStpId"`
+	PrevStpNm string   `json:"prevStpNm"`
+	PrevStpTm string   `json:"prevStpTm"`
+	IsSch     string   `json:"isSch"`
+	IsDly     string   `json:"isDly"`
+	IsFlt     string   `json:"isFlt"`
+	IsBoe     string   `json:"isBoe"`
+	IsDet     string   `json:"isDet"`
+	Flags     []string `json:"flags"`
+}
+
+type PositionsResponse struct {
+	Ctatt CtattPos
+}
+
+type CtattPos struct {
+	Tmst  time.Time
+	ErrCd string
+	ErrNm string
+	Route []RoutePosition
+}
+
+type RoutePosition struct {
+	Rt       string
+	Position []TrainPosition
+}
+
+type TrainPosition struct {
+	TrainId   string
+	RunNumber string
+	Rt        string
+	TrDr      int16
+	Lat       float64
+	Lon       float64
+	Heading   int16
+	NextStpId string
+	NextStpNm string
+	NextStpTm *time.Time
+	PrevStpId string
+	PrevStpNm string
+	PrevStpTm *time.Time
+	IsSch     bool
+	IsDly     bool
+	IsFlt     bool
+	IsBoe     bool
+	IsDet     bool
+	Flags     []string
+}
+
+type FollowProps struct {
+	RunNumber string
+}
+
+const followPath = "/api/1.0/ttfollow.aspx"
+
+type FollowApiResponse struct {
+	Ctatt struct {
+		Tmst   string        `json:"tmst"`
+		ErrCd  string        `json:"errCd"`
+		ErrNm  string        `json:"errNm"`
+		Run   string        `json:"run"`
+		Rt    string        `json:"rt"`
+		DestNm string        `json:"destNm"`
+		Route ApiFollowRoute `json:"route"`
+	} `json:"ctatt"`
+}
+
+type ApiFollowRoute struct {
+	Position []ApiFollowPosition `json:"position"`
+}
+
+type ApiFollowPosition struct {
+	StpId   string `json:"stpId"`
+	StpNm   string `json:"stpNm"`
+	StpDe   string `json:"stpDe"`
+	Prdt    string `json:"prdt"`
+	ArrT    string `json:"arrT"`
+	IsApp   string `json:"isApp"`
+	IsSch   string `json:"isSch"`
+	IsDly   string `json:"isDly"`
+	IsFlt   string `json:"isFlt"`
+}
+
+type FollowResponse struct {
+	Ctatt FollowCtatt
+}
+
+type FollowCtatt struct {
+	Tmst    time.Time
+	ErrCd   string
+	ErrNm   string
+	Run    string
+	Rt     string
+	DestNm string
+	Route  []FollowPosition
+}
+
+type FollowPosition struct {
+	StpId   string
+	StpNm   string
+	StpDe   string
+	Prdt   *time.Time
+	ArrT   *time.Time
+	IsApp  bool
+	IsSch  bool
+	IsDly  bool
+	IsFlt  bool
+}
